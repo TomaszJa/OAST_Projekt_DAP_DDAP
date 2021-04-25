8f393af0c49e9c4a23df3f5f3e21518a82725ee6
Tomi <t.jaszcz@stud.elka.pw.edu.pl>
2021-04-25 17:06:24 +0200

    Dodaj pliki projektów.

---
+++ OAST_Projekt_DAP_DDAP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OAST_Projekt_DAP_DDAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
